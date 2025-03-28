8f51a2e0876d1b5db70b3908087936c8c1309a13
github-action-benchmark <github@users.noreply.github.com>
2025-03-28 03:20:18 +0000

    add Operations.PubSubOperations (windows-latest  net9.0 Release) (benchmarkdotnet) benchmark result for f502ed8a9b29618825bac2870df2a487aae20e75

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743131967937,
+  "lastUpdate": 1743132018633,
   "repoUrl": "https://github.com/badrishc/garnet",
   "entries": {
     "Benchmark": [
@@ -40464,6 +40464,66 @@ window.BENCHMARK_DATA = {
             "value": 17460.503896077473,
             "unit": "ns",
             "range": "± 18.24953544702531"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "badrish@gmail.com",
+            "name": "Badrish Chandramouli",
+            "username": "badrishc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f502ed8a9b29618825bac2870df2a487aae20e75",
+          "message": "Update README.md",
+          "timestamp": "2025-03-27T19:23:26-07:00",
+          "tree_id": "b6003015e3a89d230b397ce08e94bb63ad44c485",
+          "url": "https://github.com/badrishc/garnet/commit/f502ed8a9b29618825bac2870df2a487aae20e75"
+        },
+        "date": 1743132006769,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 16878.65469796317,
+            "unit": "ns",
+            "range": "± 15.149933922763221"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: ACL)",
+            "value": 16690.85024320162,
+            "unit": "ns",
+            "range": "± 18.77796478429457"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 16891.144268329328,
+            "unit": "ns",
+            "range": "± 22.82000332442164"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: AOF)",
+            "value": 16723.131713867188,
+            "unit": "ns",
+            "range": "± 16.609270053711917"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 17093.83744459886,
+            "unit": "ns",
+            "range": "± 26.542699900145614"
+          },
+          {
+            "name": "BDN.benchmark.Operations.PubSubOperations.Publish(Params: None)",
+            "value": 16895.15609741211,
+            "unit": "ns",
+            "range": "± 10.6375143106377"
           }
         ]
       }
