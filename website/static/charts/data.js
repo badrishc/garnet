e96046811c14d8fd367c4f1904b2322e6ed45f86
github-action-benchmark <github@users.noreply.github.com>
2025-03-28 06:05:15 +0000

    add Operations.BasicOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 0941ebdce88b844739e1fa523b625ab66b1c032b

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743141912515,
+  "lastUpdate": 1743141915120,
   "repoUrl": "https://github.com/badrishc/garnet",
   "entries": {
     "Benchmark": [
@@ -11924,6 +11924,66 @@ window.BENCHMARK_DATA = {
             "value": 1799.3774854219878,
             "unit": "ns",
             "range": "± 2.898738069906201"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "badrish@gmail.com",
+            "name": "Badrish Chandramouli",
+            "username": "badrishc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0941ebdce88b844739e1fa523b625ab66b1c032b",
+          "message": "Update papers.md",
+          "timestamp": "2025-03-27T22:23:05-07:00",
+          "tree_id": "7dac66f7da6aaf6c008d68df16c06172e0961ece",
+          "url": "https://github.com/badrishc/garnet/commit/0941ebdce88b844739e1fa523b625ab66b1c032b"
+        },
+        "date": 1743141896451,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1800.0540052141462,
+            "unit": "ns",
+            "range": "± 1.3532848904666652"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1674.129031254695,
+            "unit": "ns",
+            "range": "± 1.3050317867534924"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1797.5613975524902,
+            "unit": "ns",
+            "range": "± 1.5926536059825647"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1796.8443577106182,
+            "unit": "ns",
+            "range": "± 1.0821772254615598"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1766.1977704366047,
+            "unit": "ns",
+            "range": "± 1.4518898710150379"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1797.641487121582,
+            "unit": "ns",
+            "range": "± 1.3125351334584872"
           }
         ]
       }
