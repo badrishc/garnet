48343fdbc69d9dcf623e2c90878395393387bc63
github-action-benchmark <github@users.noreply.github.com>
2025-03-28 03:14:15 +0000

    add Operations.ObjectOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for f502ed8a9b29618825bac2870df2a487aae20e75

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743131643429,
+  "lastUpdate": 1743131655217,
   "repoUrl": "https://github.com/badrishc/garnet",
   "entries": {
     "Benchmark": [
@@ -15532,6 +15532,66 @@ window.BENCHMARK_DATA = {
             "value": 92072.37386067708,
             "unit": "ns",
             "range": "± 287.101960002718"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "badrish@gmail.com",
+            "name": "Badrish Chandramouli",
+            "username": "badrishc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f502ed8a9b29618825bac2870df2a487aae20e75",
+          "message": "Update README.md",
+          "timestamp": "2025-03-27T19:23:26-07:00",
+          "tree_id": "b6003015e3a89d230b397ce08e94bb63ad44c485",
+          "url": "https://github.com/badrishc/garnet/commit/f502ed8a9b29618825bac2870df2a487aae20e75"
+        },
+        "date": 1743131644313,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: ACL)",
+            "value": 109412.0048014323,
+            "unit": "ns",
+            "range": "± 389.72154194356557"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: ACL)",
+            "value": 94622.69287109375,
+            "unit": "ns",
+            "range": "± 932.8078733828751"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: AOF)",
+            "value": 120131.25939002403,
+            "unit": "ns",
+            "range": "± 473.24314970520976"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: AOF)",
+            "value": 105133.14819335938,
+            "unit": "ns",
+            "range": "± 625.2553235737805"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: None)",
+            "value": 108626.02945963542,
+            "unit": "ns",
+            "range": "± 138.5868781174967"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: None)",
+            "value": 95367.02357700893,
+            "unit": "ns",
+            "range": "± 434.11363028926075"
           }
         ]
       }
